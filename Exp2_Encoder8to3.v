cb964f6b924afccc15d60e4643e462b6c5df3ea7
MANOJKUMAR282004 <167055290+MANOJKUMAR282004@users.noreply.github.com>
2024-05-11 14:00:11 +0530

    Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,21 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
-output a,b,c;
 
+module demux_1_8(y,s,a);
+output reg [7:0]y;
+input [2:0]s;
+input a;
 
-
+always @(*)
+begin 
+y=0;
+case(s)
+3'd0: y[0]=a;
+3'd1: y[1]=a;
+3'd2: y[2]=a;
+3'd3: y[3]=a;
+3'd4: y[4]=a;
+3'd5: y[5]=a;
+3'd6: y[6]=a;
+3'd7: y[7]=a;
+endcase
+end
 endmodule
